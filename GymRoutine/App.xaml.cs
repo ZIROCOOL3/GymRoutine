17945b2cff7411b1ebbe1840e3e7019dee8d9124
zirocool2020 <zirocool2003@hotmail.com>
2023-06-23 13:30:08 -0300

    --

---
+++ GymRoutine/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GymRoutine;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ GymRoutine/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GymRoutine;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
